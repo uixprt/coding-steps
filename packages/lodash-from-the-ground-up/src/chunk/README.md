c00ac5c758228c466439e4c2165db37701f033dc
Eyal Lapid <eyall@proftit.com>
2021-05-05 22:18:01 +0300

    add lodash from the ground up starter

---
+++ packages/lodash-from-the-ground-up/src/chunk/README.md
@@ -0,0 +1,14 @@
+# chunk
+
+Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+
+## Example
+
+```js
+_.chunk(['a', 'b', 'c', 'd'], 2);
+// => [['a', 'b'], ['c', 'd']]
+ 
+_.chunk(['a', 'b', 'c', 'd'], 3);
+// => [['a', 'b', 'c'], ['d']]
+```
+
---
+++ packages/lodash-from-the-ground-up/src/chunk/chunk.solution-example.ts
@@ -0,0 +1,23 @@
+export function chunk(array, chunkSize) {
+  const firstChunk = [];
+
+  const {chunksList} = array.reduce(({ chunksList, chunk }, item) => {
+    if (chunk.length >= chunkSize) {
+      const newChunk = [item];
+
+      return {
+        chunksList: [...chunksList, newChunk],
+        chunk: newChunk,
+      };
+    }
+
+    chunk.push(item);
+
+    return {
+      chunksList,
+      chunk,
+    };
+  }, { chunksList: [firstChunk], chunk: firstChunk });
+
+  return chunksList;
+}
