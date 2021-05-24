aa1667f8d1b53b199abd6f853bc52a2bc2ed0a46
Amit Barletz <amitbarletz@outlook.com>
2021-05-24 21:11:02 +0300

    add logic

--- packages/rxjs-from-the-ground-up/src/debounce-time/solutions/debounce-time.solution.veganzard.ts
+++ packages/rxjs-from-the-ground-up/src/debounce-time/solutions/debounce-time.solution.veganzard.ts
@@ -1,1 +1,30 @@
-export function debounceTime() {}
+import { SchedulerLike } from "../../types/scheduler-like";
+import { createObservable } from "../../create-observable/solutions/create-observable.solution.starosaur";
+import { Observable } from "../../types/observable";
+
+export function debounceTime(dueTime: number, scheduler: SchedulerLike) {
+  return (source$: Observable) => {
+    return createObservable((subscriber) => {
+      let timeoutId: number;
+      const sub = source$.subscribe({
+        next: (value) => {
+          if (timeoutId) {
+            clearTimeout(timeoutId);
+          } else {
+            timeoutId = setTimeout(() => {
+              subscriber.next(value);
+            }, dueTime);
+          }
+        },
+        error: (err: Error) => {
+          subscriber.error(err);
+        },
+        complete: () => {
+          subscriber.complete();
+        },
+      });
+
+      return () => sub.unsubscribe;
+    });
+  };
+}
